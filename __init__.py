ebd2ff6f5558fa79733f6eb3bd401f26868f55fc
hugools <74406394+hugools@users.noreply.github.com>
2025-03-04 14:01:53 +0100

    Update and rename __init__.py to animals/__init__.py

--- __init__.py
+++
@@ -1,1 +0,0 @@
-from .harmless import Birds, Mammals, Fishfrom .dangerous import Birds, Mammals, Fish
---
+++ animals/__init__.py
@@ -0,0 +1,2 @@
+from .harmless import Birds, Mammals, Fish
+from .dangerous import Birds, Mammals, Fish
